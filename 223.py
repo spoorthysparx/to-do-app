12bcb13bfb15b194c6f0f2273242111114bf41d5
spoorthysparx <71026441+spoorthysparx@users.noreply.github.com>
2020-10-27 20:01:48 +0530

    version 2

--- 223.py
+++ 223.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 layout=[
     [sg.Text("enter your choice",font=("Arial",16)),
-    sg.InputText("",size=(20,1),font=("Arial",16),key="choice"),sg.Button("Add",key="add_save")],
+    sg.InputText("",size=(20,1),font=("Arial",16),key="choice"),sg.Button("Add",key="add_save"),sg.Button("save task",key="save_tasks"),sg.Button("show tasks",key="show_tasks")],
     [sg.Listbox([],size=(40,5),font=("Arial",16),key="tasks"),
     sg.Button("Edit",key="edit"),sg.Button("Delete",key="delete"),],[sg.Button("exit",key="exit")]
 ]
@@ -26,6 +26,20 @@ def edit_tasks(values):
     window.FindElement('add_save').update('save')
 
 todolist=[]
+def save_tasks():
+    with open('122.txt','a') as f:
+        for i in todolist:
+                f.write(i+"\n")
+    todolist.clear()  
+
+def show_tasks():
+    with open('122.txt','r') as f:
+            task=f.readlines()
+            for i in task:
+                if i not in todolist:
+                    todolist.append(i)
+    window.FindElement("tasks").update(todolist)
+
 
 window= sg.Window("first app",layout)
 while True:
@@ -36,6 +50,11 @@ while True:
         delete_tasks(values)
     elif event=="edit":
         edit_tasks(values)
+    elif event == "save_tasks":
+        save_tasks()
+    elif event=="show_tasks":
+        show_tasks()
     else:
         break
+
 window.close()
